64fe696734cb6e6179ca73c55b6c753c633a5e3f
Victor Moura <victor_cmoura@hotmail.com>
2018-10-14 15:42:08 -0300

    Adds fact to centralize queries and reestructures interaction
    Co-authored-by: Arthur Diniz <arthurbdiniz@gmail.com>
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Guilherme Lacerda <guiglacerda@gmail.com>

--- prodict.pl
+++ prodict.pl
@@ -1,6 +1,6 @@
 :- style_check(-singleton).
 :- dynamic profile/2.
-:- dynamic engineering_course/12.
+:- dynamic engineering_course/13.
 :- dynamic clearBase/1.
 :- dynamic clearBase1/1.
 :- dynamic play/0.
@@ -18,22 +18,13 @@
 :- dynamic user_wants_publicuniversities/0.
 :- dynamic user_likes_computers/0.
 
-%symbolic fact
-profile(nothing, y_n).
-
-clear_base(X):- clear_base1(X), fail.
-  clear_base(X).
-
-clear_base1(X):- retract(X).
-  clear_base1(X).
-
 % Name, Math, Transport, Building, Product, Automation, Studies, Nature, Military, Hardware, Electronics, PublicUniversities, Computer
 
-engineering_course('Engenharia Aeronautica', y, y, n, y, n, y, n, y, y, n, y, n).
-% engineering_course('Engenharia Ambiental', ).
-% engineering_course('Engenharia Cartografica', ).
-% engineering_course('Engenharia da Computação', ).
-% engineering_course('Engenharia de Alimentos', ).
+engineering_course('Engenharia Aeronautica', s, s, n, s, n, s, n, s, s, n, s, n).
+engineering_course('Engenharia Ambiental', s, n, s, n, s, s, s, n, s, n, s, n).
+engineering_course('Engenharia Cartografica', s, n, n, n, n, s, s, s, s, n, s, n).
+engineering_course('Engenharia da Computação', s, n, n, n, s, n, n, n, s, s, s, s).
+engineering_course('Engenharia de Alimentos', s, n, n, s, n, n, s, n, s, n, s, n).
 % engineering_course('Engenharia de Controle e Automacao', ).
 % engineering_course('Engenharia de Horticultura', ).
 % engineering_course('Engenharia de Minas', ).
@@ -65,76 +56,63 @@ engineering_course('Engenharia Aeronautica', y, y, n, y, n, y, n, y, y, n, y, n)
 % engineering_course('Engenharia Textil', ).
 
 start_game :-
-  clear_base(profile(profile, Yn)),
   nl, write('Responda as perguntas e saiba quais engenharias mais se parecem com voce!'), nl, nl,
-  user_likes_math.
+  user_likes_math.  
 
-start_game(y) :-
-  play.
+end_round :-
+  nl, nl, write('Boa sorte na sua escolha!'), nl, nl, !, halt.
 
-start_game(n) :-
-  nl, write('Boa sorte na sua escolha!'), nl, nl, !.
-
-new_round :-
-  nl, nl, write('Gostou do resultado? Voce pode tentar novamente: (y/n)? '),
-	read(Desire),
-	play(Desire).
-
+query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, LikesComputers):- 
+  findall(Y, engineering_course(Y, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, LikesComputers), L),
+  length(L, N), N == 0,
+  write('Não consegui encontrar uma engenharia para você =('),
+  end_round;
+  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, LikesComputers), L),
+  length(L, N), N @=< 3,
+  write('São grandes as possibilidades de você gostar de: '), nl,
+  forall(engineering_course(Name, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, LikesComputers), (write('- '), write(Name), nl)),
+  end_round.
+  
 user_likes_math :-
-  write('Voce gosta de exatas (y/n)? '),
+  write('Voce gosta de exatas (s/n)? '),
   read(Answer),
   asserta(profile(likesMath, Answer)),
   profile(likesMath, LikesMath),
-  findall(X, engineering_course(X, LikesMath, _, _, _, _, _, _, _, _, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, _, _, _, _, _, _, _, _, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, _, _, _, _, _, _, _, _, _, _, _);
   user_likes_transports.
 
 user_likes_transports :-
-  profile(likesMath, n), asserta(profile(likesTransports, n)), fail;
-  profile(likesMath, y),
-  write('Voce gosta do ramo de transportes (y/n)? '),
+  write('Voce gosta do ramo de transportes (s/n)? '),
   read(Answer),
   asserta(profile(likesTransports, Answer)),
   profile(likesMath, LikesMath),
   profile(likesTransports, LikesTransports),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, _, _, _, _, _, _, _, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, _, _, _, _, _, _, _, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
-  profile(likesMath, n), retract(profile(likesMath, n)), user_likes_math;
+  query_possibilities(LikesMath, LikesTransports, _, _, _, _, _, _, _, _, _, _);
   user_likes_buildings.
 
 user_likes_buildings :-
-  write('Voce gosta da area de estruturas (y/n)? '),
+  write('Voce gosta da area de estruturas (s/n)? '),
   read(Answer),
   asserta(profile(likesBuildings, Answer)),
   profile(likesMath, LikesMath),
   profile(likesTransports, LikesTransports),
   profile(likesBuildings, LikesBuildings),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, _, _, _, _, _, _, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, _, _, _, _, _, _, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, _, _, _, _, _, _, _, _, _);
   user_likes_products.
 
 user_likes_products :-
-  write('Voce se interessa por engenharia de produtos (y/n)? '),
+  write('Voce se interessa por engenharia de produtos (s/n)? '),
   read(Answer),
   asserta(profile(likesProducts, Answer)),
   profile(likesMath, LikesMath),
   profile(likesTransports, LikesTransports),
   profile(likesBuildings, LikesBuildings),
   profile(likesProducts, LikesProducts),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, _, _, _, _, _, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, _, _, _, _, _, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, _, _, _, _, _, _, _, _);
   user_likes_automation.
 
 user_likes_automation :-
-  write('Voce se interessa por robotica (y/n)? '),
+  write('Voce se interessa por robotica (s/n)? '),
   read(Answer),
   asserta(profile(likesAutomation, Answer)),
   profile(likesMath, LikesMath),
@@ -142,14 +120,11 @@ user_likes_automation :-
   profile(likesBuildings, LikesBuildings),
   profile(likesProducts, LikesProducts),
   profile(likesAutomation, LikesAutomation),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, _, _, _, _, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, _, _, _, _, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, _, _, _, _, _, _, _);
   user_likes_studies.
 
 user_likes_studies :-
-  write('Voce prefere realizar estudos no lugar de produtos (y/n)? '),
+  write('Voce prefere realizar estudos no lugar de produtos (s/n)? '),
   read(Answer),
   asserta(profile(likesStudies, Answer)),
   profile(likesMath, LikesMath),
@@ -158,14 +133,11 @@ user_likes_studies :-
   profile(likesProducts, LikesProducts),
   profile(likesAutomation, LikesAutomation),
   profile(likesStudies, LikesStudies),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, _, _, _, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, _, _, _, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, _, _, _, _, _, _);
   user_likes_nature.
 
 user_likes_nature :-
-  write('Voce gosta de interagir com a natureza (y/n)? '),
+  write('Voce gosta de interagir com a natureza (s/n)? '),
   read(Answer),
   asserta(profile(likesNature, Answer)),
   profile(likesMath, LikesMath),
@@ -175,14 +147,11 @@ user_likes_nature :-
   profile(likesAutomation, LikesAutomation),
   profile(likesStudies, LikesStudies),
   profile(likesNature, LikesNature),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, _, _, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, _, _, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, _, _, _, _, _);
   user_likes_military.
 
 user_likes_military :-
-  write('Voce simpatiza com instituicoes militares (y/n)? '),
+  write('Voce simpatiza com instituicoes militares (s/n)? '),
   read(Answer),
   asserta(profile(likesMilitary, Answer)),
   profile(likesMath, LikesMath),
@@ -193,14 +162,11 @@ user_likes_military :-
   profile(likesStudies, LikesStudies),
   profile(likesNature, LikesNature),
   profile(likesMilitary, LikesMilitary),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, _, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, _, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, _, _, _, _);
   user_likes_hardware.
 
 user_likes_hardware :-
-  write('Voce gostaria de construir coisas concretas, nao virtuais (y/n)? '),
+  write('Voce gostaria de construir coisas concretas, nao virtuais (s/n)? '),
   read(Answer),
   asserta(profile(likesHardware, Answer)),
   profile(likesMath, LikesMath),
@@ -212,14 +178,11 @@ user_likes_hardware :-
   profile(likesNature, LikesNature),
   profile(likesMilitary, LikesMilitary),
   profile(likesHardware, LikesHardware),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, _, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, _, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, _, _, _);
   user_likes_electronics.
 
 user_likes_electronics :-
-  write('Voce se interessa por eletronicos (y/n)? '),
+  write('Voce se interessa por eletronicos (s/n)? '),
   read(Answer),
   asserta(profile(likesElectronics, Answer)),
   profile(likesMath, LikesMath),
@@ -232,14 +195,11 @@ user_likes_electronics :-
   profile(likesMilitary, LikesMilitary),
   profile(likesHardware, LikesHardware),
   profile(likesElectronics, LikesElectronics),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, _, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, _, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, _, _);
   user_wants_publicuniversities.
 
 user_wants_publicuniversities :-
-  write('Voce pretende cursar em uma instituicao publica (y/n)? '),
+  write('Voce pretende cursar em uma instituicao publica (s/n)? '),
   read(Answer),
   asserta(profile(love, Answer)),
   profile(likesMath, LikesMath),
@@ -253,14 +213,11 @@ user_wants_publicuniversities :-
   profile(likesHardware, LikesHardware),
   profile(likesElectronics, LikesElectronics),
   profile(love, LikesPublicUniversities),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, _), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, _),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, _);
   user_likes_computers.
 
 user_likes_computers :-
-  write('Voce gosta de entender o funcionamento de computadores (y/n)? '),
+  write('Voce gosta de entender o funcionamento de computadores (s/n)? '),
   read(Answer),
   asserta(profile(honor, Answer)),
   profile(likesMath, LikesMath),
@@ -275,8 +232,7 @@ user_likes_computers :-
   profile(likesElectronics, LikesElectronics),
   profile(love, LikesPublicUniversities),
   profile(honor, LikesComputers),
-  findall(X, engineering_course(X, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, LikesComputers), L),
-  length(L, N), N == 1,
-  engineering_course(Z, LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, LikesComputers),
-  write('Hmm... Eu acho que... '), write(Z), write('!'), new_round;
+  query_possibilities(LikesMath, LikesTransports, LikesBuildings, LikesProducts, LikesAutomation, LikesStudies, LikesNature, LikesMilitary, LikesHardware, LikesElectronics, LikesPublicUniversities, LikesComputers);
   write('Arghhh!!! Não!!!'), new_round.
+
+?- start_game.
